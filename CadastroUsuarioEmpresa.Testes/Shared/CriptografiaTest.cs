0952418e286d41957d046f260e13a606a3f1da97
Edi Not alugado <edileal1@gmail.com>
2022-11-06 22:58:48 -0300

    Commit dos testes

---
+++ CadastroUsuarioEmpresa.Testes/Shared/CriptografiaTest.cs
@@ -0,0 +1,21 @@
+﻿using CadastroUsuarioEmpresa.Domain.Shared;
+using CadastroUsuarioEmpresa.Services;
+using CadastroUsuarioEmpresa.Testes.Fakers;
+using System.Security.Cryptography;
+using Xunit;
+
+namespace CadastroUsuarioEmpresa.Testes.Shared
+{
+    public class CriptografiaTest
+    {
+        [Fact(DisplayName = "Testes criptografia ok")]
+        public void TesteSeCriptografiaOk()
+        {
+            string senha = "Dado123-";
+            string senhaCriptografada = Cryptography.Encrypt(senha);
+            string senhaDescriptografada = Cryptography.Decrypt(senhaCriptografada);
+
+            Assert.Equal(senhaDescriptografada, senha);
+        }
+    }
+}
